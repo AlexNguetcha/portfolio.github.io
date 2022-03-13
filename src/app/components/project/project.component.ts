8cf3e67cf6a62e2d4dcee6be08a7238b5863f92c
alexnguetcha <nguetchaalex@gmail.com>
2022-03-14 00:16:48 +0100

    add project

--- src/app/components/project/project.component.ts
+++ src/app/components/project/project.component.ts
@@ -12,12 +12,37 @@ export class ProjectComponent implements OnInit {
     this.projects = [
       {
         name: "Angular Portfolio",
-        description: "A powerful angular portfolio without a css framework.",
-        picture: "../../../assets/logos/html.svg",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis vitae ipsum nam debitis, laudantium sapiente ipsa a quam, ratione asperiores maxime aspernatur possimus autem itaque eligendi eius explicabo beatae qui!",
+        picture: "../../../assets/project/portfolio.png",
+        technos : ["angular", "typescript", "html", "css"],
+        github: "https://github.com/AlexNguetcha/portfolio",
+        link: "https://dashboard.heroku.com/apps"
+      },
+      {
+        name: "Angular Portfolio",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis vitae ipsum nam debitis, laudantium sapiente ipsa a quam, ratione asperiores maxime aspernatur possimus autem itaque eligendi eius explicabo beatae qui!",
+        picture: "../../../assets/project/portfolio.png",
+        technos : ["angular", "typescript", "html", "css"],
+        github: "https://github.com/AlexNguetcha/portfolio",
+        link: "https://dashboard.heroku.com/apps"
+      },
+      {
+        name: "Angular Portfolio",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis vitae ipsum nam debitis, laudantium sapiente ipsa a quam, ratione asperiores maxime aspernatur possimus autem itaque eligendi eius explicabo beatae qui!",
+        picture: "../../../assets/project/portfolio.png",
+        technos : ["angular", "typescript", "html", "css"],
+        github: "https://github.com/AlexNguetcha/portfolio",
+        link: "https://dashboard.heroku.com/apps"
+      },
+      {
+        name: "Angular Portfolio",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis vitae ipsum nam debitis, laudantium sapiente ipsa a quam, ratione asperiores maxime aspernatur possimus autem itaque eligendi eius explicabo beatae qui!",
+        picture: "../../../assets/project/portfolio.png",
         technos : ["angular", "typescript", "html", "css"],
         github: "https://github.com/AlexNguetcha/portfolio",
         link: "https://dashboard.heroku.com/apps"
       }
+      
     ];
   }
 
